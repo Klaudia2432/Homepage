f04acc8943178e1aae12ad31cbf44584b9805b9b
Klaudia2432 <klaudiadziubek2432@gmail.com>
2024-11-01 11:01:21 +0000

    Added the functions with JS

--- script.js
+++ script.js
@@ -6,3 +6,15 @@ console.log(sectionDescription.innerHTML)
 sectionDescription.innerHTML = 'WTF - What the frontend course'
 sectionDescription.style.color = 'darkgreen'
 console.log(sectionDescription.innerHTML)
+
+function sayHello(user, age) {
+    return `Hello ${user} and you are ${age} years old`
+}
+
+console.log(sayHello('Klaudia', 27))
+
+sayHello = (user, age) => {
+    return `Hello ${user} and you are ${age} years old too`
+}
+
+console.log(sayHello('Klaudia', 27))
