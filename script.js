be472f914e24868d97168553fe359c30496f11fc
Klaudia2432 <klaudiadziubek2432@gmail.com>
2024-11-13 12:00:49 +0000

    Added hamburger menu to the other pages too

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const welcomeText = "Hello, future Frontend developer!";
 console.log(welcomeText);
 
-const sectionDescription = document.querySelector('.section__description')
-console.log(sectionDescription.innerHTML)
-sectionDescription.innerHTML = 'WTF - What the frontend course'
-sectionDescription.style.color = 'darkgreen'
-console.log(sectionDescription.innerHTML)
+// const sectionDescription = document.querySelector('.section__description')
+// console.log(sectionDescription.innerHTML)
+// sectionDescription.innerHTML = 'WTF - What the frontend course'
+// sectionDescription.style.color = 'darkgreen'
+// console.log(sectionDescription.innerHTML)
 
 function sayHello(user, age) {
     return `Hello ${user} and you are ${age} years old`
